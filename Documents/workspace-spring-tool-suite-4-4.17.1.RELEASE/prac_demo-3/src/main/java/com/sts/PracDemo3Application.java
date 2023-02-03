bfdb8c80b43a3a041cd71d99dc2069a73033f25a
Priyanshi Srivastava <priyanshi.srivastava@nagarro.com>
2023-02-03 10:46:46 +0530

    Docker Assignment:

---
+++ Documents/workspace-spring-tool-suite-4-4.17.1.RELEASE/prac_demo-3/src/main/java/com/sts/PracDemo3Application.java
@@ -0,0 +1,13 @@
+package com.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracDemo3Application.class, args);
+	}
+
+}
